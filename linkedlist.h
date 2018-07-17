54774f5c7e578869343241fafdbedbcc0ff083ba
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-07-18 00:08:44 +0500

    Added some code

--- linkedlist.h
+++ linkedlist.h
@@ -7,6 +7,7 @@
 
 void copystr( char *dest, const char *src );
 
+/*
 struct LinkedListItem {
     char key[16];
     char value[16];
@@ -30,23 +31,76 @@ struct LinkedListItem {
         return *this;
     };
 };
+*/
 
+template <class T>
 class LinkedList {
+    public:
+        struct LinkedListIt {
+            T *data;
+            LinkedListIt *next;
+        };
+        LinkedList() {
+            m_first = NULL;
+            m_last = NULL;
+            m_num_items = 0;
+        };
+        ~LinkedList() {
+            LinkedListIt *it = m_first;
+            LinkedListIt *item = NULL;
+            while ( it != NULL ) {
+                item = it;
+                delete item;
+                it = it->next;
+            }
+        }
+        int size() {
+            return m_num_items;
+        };
+        LinkedListIt& first() {
+            return *m_first;
+        };
+        LinkedListIt& last() {
+            return *m_last;
+        };
+        T& at(const int index) {
+            LinkedListIt *found = NULL;
+            LinkedListIt *it = m_first;
+            int curr_id = 0;
+            while ( curr_id < index ) {
+                it = it->next;
+                curr_id++;
+                if ( it == NULL ) break;
+            };
+            if ( it != NULL ) {
+                found = it;
+            };
+            return *found->data;
+        };
+        void add( T &data ) {
+            LinkedListIt *item = new LinkedListIt();
+            item->data = new T(data);
+            if ( m_first == NULL ) {
+                m_first = item;
+                m_last = item;
+            } else {
+                m_last->next = item;
+                m_last = item;
+            };
+            item->next = NULL;
+            m_num_items++;
+        };
+        void printContent() {
+            LinkedListIt *begin = m_first;
+            while ( begin != NULL ) {
+                begin->data->print();
+                begin = begin->next;
+            };
+        };
     private:
         int m_num_items;
-        LinkedListItem *m_first;
-        LinkedListItem *m_last;
-    public:
-        LinkedList();
-        ~LinkedList();
-        int size();
-        LinkedListItem& first();
-        LinkedListItem& last();
-        LinkedListItem& at(const int index);
-        void add( LinkedListItem *item );
-        void add( const char *key, const char *value );
-        void printContent();
+        LinkedListIt *m_first;
+        LinkedListIt *m_last;
 };
-
 #endif // __LINKEDLIST_H__
 
--- test_ll.cpp
+++ test_ll.cpp
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include "linkedlist.h"
 
+struct Item {
+    char key[20];
+    char value[20];
+    void print() {
+        printf(" >> %s = %s\n", key, value);
+    };
+    Item(const Item &src) {
+        *this = src;
+    };
+    Item(const char *key, const char *value) {
+        strcpy(this->key, key);
+        strcpy(this->value, value);
+    };
+    Item& operator=(const Item &src) {
+        strcpy(this->key, src.key);
+        strcpy(this->value, src.value); 
+        return *this;
+    };
+};
+
 
 int main() {
-    LinkedList l;
+    LinkedList<Item> l;
+    LinkedList<char*> c;
     printf("List size %d items\n", l.size()); 
-    l.add("one", "oneone");
-    l.add("two", "twotwo");
-    l.add("three", "threethree");
-    l.add("four", "fourfour");
-    l.add("five", "fivefive");
+/*
+    l.add(Item("one", "oneone"));
+    l.add(Item("two", "twotwo"));
+    l.add(Item("three", "threethree"));
+//    l.add(Item("four", "fourfour"));
+//    l.add(Item("five", "fivefive"));
     printf("List size %d items\n", l.size()); 
     l.printContent();
-    l.at(5);
+    l.at(0).print();
+*/
+    c.add("char one");
+    c.add("char two");
     return 0;
 }
 
